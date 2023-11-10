737eac4b7fbe2771cbb2e55731a96d0234dd3006
stephenmdevine <stephenmdevine@gmail.com>
2023-11-09 18:54:31 -0600

    Two tests passed.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", event => {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+    });
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -18,6 +18,13 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
+    if (testInput === "") {
+        return "Empty";
+    }   else if (!isNaN(testInput)) {
+        return "Is a Number";
+    }   else {
+        return "Not a Number";
+    }
     
  }
  
